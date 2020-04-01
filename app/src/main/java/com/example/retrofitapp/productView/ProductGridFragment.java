3e490e5a5cdf3c5ea7d4cc50088b62d262d64735
servismas <servismas@gmail.com>
2020-04-01 16:20:26 +0300

    Add RecyclerView products

--- app/src/main/java/com/example/retrofitapp/productView/ProductGridFragment.java
+++ app/src/main/java/com/example/retrofitapp/productView/ProductGridFragment.java
@@ -3,12 +3,17 @@ package com.example.retrofitapp.productView;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.retrofitapp.R;
+import com.example.retrofitapp.network.ProductEntry;
+
+import java.util.List;
 
 
 public class ProductGridFragment extends Fragment {
@@ -24,6 +29,23 @@ public class ProductGridFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_product_grid, container, false);
+        View view = inflater.inflate(R.layout.fragment_product_grid, container, false);
+
+        // Set up the RecyclerView
+        RecyclerView recyclerView = view.findViewById(R.id.recycler_view);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new GridLayoutManager(getContext(), 2,
+                GridLayoutManager.VERTICAL, false));
+
+        List<ProductEntry> list = ProductEntry.initProductEntryList(getResources());
+        ProductCardRecyclerViewAdapter adapter = new ProductCardRecyclerViewAdapter(list);
+
+        recyclerView.setAdapter(adapter);
+
+//        int largePadding = getResources().getDimensionPixelSize(R.dimen.shr_product_grid_spacing);
+//        int smallPadding = getResources().getDimensionPixelSize(R.dimen.shr_product_grid_spacing_small);
+//        recyclerView.addItemDecoration(new ProductGridItemDecoration(largePadding, smallPadding));
+
+        return view;
     }
 }
